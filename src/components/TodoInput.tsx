360c716042f5dfdf7d8f753dedcd17c9015cc929
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-09-30 23:14:05 -0300

    Challenge done

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,10 +7,10 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
-  function handleAddNewTask() {
-    //TODO - Call addTask if task not empty and clean input value 
+  function handleAddNewTask(taskText: string) {
+    setTask(taskText)
   }
 
   return (
@@ -21,13 +21,13 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
-        //TODO - use value, onChangeText and onSubmitEditing props
+        onChangeText={handleAddNewTask}
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
+        onPress={() => addTask(task)}
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
       </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,33 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const taskIndex = tasks.findIndex(task => task.id === id);
+
+    const newTasks = tasks.map(task => {
+      if(task.id !== id) return task;
+
+      task.done = !task.done;
+
+      return task
+    })
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksFiltered = tasks.filter(task => task.id !== id);
+
+    setTasks(tasksFiltered);
   }
 
   return (
